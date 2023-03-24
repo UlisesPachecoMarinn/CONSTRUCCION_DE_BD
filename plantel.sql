bcafeb61d1f09e3909fa7267faf954478f88acc1
UlisesPachecoMarinn <127709200+UlisesPachecoMarinn@users.noreply.github.com>
2023-03-23 22:30:38 -0600

    Add files via upload

---
+++ plantel.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-03-2023 a las 05:24:39
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_405`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plantel`
+--
+
+CREATE TABLE `plantel` (
+  `id_PLANTEL` int(11) NOT NULL,
+  `NO_PLANTEL` varchar(55) NOT NULL,
+  `DIRECCION` varchar(55) NOT NULL,
+  `CLAVE` int(55) NOT NULL,
+  `TEL_PLANTEL` varchar(55) NOT NULL,
+  `EMAIL_PLANTEL` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `plantel`
+--
+ALTER TABLE `plantel`
+  ADD PRIMARY KEY (`id_PLANTEL`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `plantel`
+--
+ALTER TABLE `plantel`
+  MODIFY `id_PLANTEL` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
